94edc4df00576b02ae70560800b26746e9551cf5
xsaven <gms.sw.xsaven@gmail.com>
2021-05-22 21:43:46 +0300

    README.md, config/scaffold.php, src/FileStorage.php, src/LevyModel/LevyModel.php, src/LevyModel/LevyRelatedTypeModel.php, src/LevyModel/LevyRelation/LevyBelongsToManyModel.php, src/LevyModel/LevyRelation/LevyBelongsToModel.php, src/LevyModel/LevyRelation/LevyHasManyModel.php, src/LevyModel/LevyRelation/LevyHasManyThroughModel.php, src/LevyModel/LevyRelation/LevyHasOneModel.php, src/LevyModel/LevyRelation/LevyHasOneThroughModel.php, src/LevyModel/LevyRelation/LevyMorphManyModel.php, src/LevyModel/LevyRelation/LevyMorphOneModel.php, src/LevyModel/LevyRelation/LevyMorphToManyModel.php, src/LevyModel/LevyRelation/LevyMorphToModel.php, src/LevyModel/LevyRelation/LevyMorphedByManyModel.php, src/LevyModel/LevyRelation/LevyRelationAbstract.php, src/Listeners/MakeModelListen.php, src/Listeners/MakeObserverListen.php

--- src/Listeners/MakeModelListen.php
+++ src/Listeners/MakeModelListen.php
@@ -2,6 +2,7 @@
 
 namespace Bfg\Scaffold\Listeners;
 
+use Bfg\Entity\Core\Entities\DocumentorEntity;
 use Bfg\Scaffold\LevyModel\LevyFieldModel;
 use Bfg\Scaffold\LevyModel\LevyModel;
 use Illuminate\Database\Eloquent\Model;
@@ -36,64 +37,72 @@ class MakeModelListen extends ListenerControl
             ->extend(Model::class);
 
         foreach ($model->traits as $trait) {
-
             $class->addTrait($trait->class);
         }
 
         foreach ($model->constants as $constant) {
-
             $class->const(strtoupper($constant->name), $this->formatString($constant->value));
         }
 
         if ($model->created && !$model->updated) {
-
             $class->const('CREATED_AT', 'null');
-
-        } else if (!$model->created && $model->updated) {
-
-            $class->const('UPDATED_AT', 'null');
+        } else {
+            if (!$model->created && $model->updated) {
+                $class->const('UPDATED_AT', 'null');
+            }
         }
 
         $class->prop('protected:table', $model->table);
 
         if (!$model->created && !$model->updated) {
-
             $class->prop('public:timestamps', false);
         }
 
         if ($model->foreign && $model->foreign != 'id') {
-
             $class->prop('protected:primaryKey', $model->foreign);
         }
 
         $attrs = $model->fields
             ->sortBy('order')
             ->pluck('default', 'name')
-            ->filter(fn ($i) => !is_null($i) && $i !== 'id' && $i !== 'created_at' && $i !== 'updated_at')
+            ->filter(fn($i) => !is_null($i) && $i !== 'id' && $i !== 'created_at' && $i !== 'updated_at')
             ->toArray();
 
         if (count($attrs)) {
-
             $class->prop('protected:attributes', $attrs);
         }
 
         $class->prop(
             'protected:fillable',
             $model->fields->sortBy('order')->pluck('name')
-                ->filter(fn ($k) => $k !== 'id' && $k !== 'created_at' && $k !== 'updated_at')->toArray()
+                ->filter(fn($k) => $k !== 'id' && $k !== 'created_at' && $k !== 'updated_at')->toArray()
         );
 
         $class->prop(
             'protected:casts',
             $model->fields->sortBy('order')->mapWithKeys(function (LevyFieldModel $model) {
                 return $model->cast ? [$model->name => $model->cast] : [];
-            })->filter(fn ($i,$k) => $k !== 'id' && $k !== 'created_at' && $k !== 'updated_at')->toArray()
+            })->filter(fn($i, $k) => $k !== 'id' && $k !== 'created_at' && $k !== 'updated_at')->toArray()
         );
 
-        foreach ($model->relations as $relation) {
+        if ($model->observer) {
 
-            $method = $class->method($relation->relation_name)->line();
-            $method->line("\$this->{$relation->name}(" . implode(', ', $this->formatArray($relation->relation->relation_params)) . ");");
+            $method = $class->method('boot')->modifier('protected static');
+            $method->doc(function (DocumentorEntity $entity) {
+                $entity->description('Bootstrap the model and its traits.');
+                $entity->tagReturn('void');
+            });
+            $method->line("parent::boot();")->line();
+            $method->line("static::observe({$model->observer->class}::class);");
+        }
+
+        foreach ($model->relations as $relation) {
+            $method = $class->method($relation->relation_name)->returnType($relation->relation->relation_class);
+            $method->doc(function (DocumentorEntity $entity) use ($relation) {
+                $entity->tagReturn($relation->relation->relation_class);
+            });
+            $method->line("return \$this->{$relation->name}(".implode(', ',
+                    $this->formatArray($relation->relation->relation_params)).");");
         }
 
         return [
--- src/Listeners/MakeObserverListen.php
+++ src/Listeners/MakeObserverListen.php
@@ -2,6 +2,8 @@
 
 namespace Bfg\Scaffold\Listeners;
 
+use Bfg\Entity\Core\Entities\ClassMethodEntity;
+use Bfg\Entity\Core\Entities\DocumentorEntity;
 use Bfg\Scaffold\LevyModel\LevyModel;
 
 /**
@@ -18,6 +20,37 @@ class MakeObserverListen extends ListenerControl
      */
     public function handle(LevyModel $model)
     {
-        
+        if ($model->observer) {
+
+            $class = class_entity($model->observer->class_name)
+                ->namespace($model->observer->namespace);
+
+            foreach ($model->observer->events as $event) {
+
+                /** @var ClassMethodEntity $method */
+
+                /**
+                 * Handle the Director "created" event.
+                 *
+                 * @param  \App\Models\Director  $director
+                 * @return void
+                 */
+
+                $method = $class->method($event);
+                $param_name = \Str::singular($model->name);
+                $method->doc(function (DocumentorEntity $entity) use ($event, $model, $param_name) {
+                    $entity->description("Handle the {$model->class_name} \"{$event}\" event.");
+                    $entity->tagParam($model->class, $param_name);
+                });
+                $method->param($param_name, null, $model->class);
+                $method->line('//');
+            }
+
+            $this->storage()->store(
+                app_path("Observers/{$model->observer->class_name}.php"),
+                $class->wrap('php')->render(),
+                false
+            );
+        }
     }
 }
