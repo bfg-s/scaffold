ecfcc3704f97792c4c8d873bef16bd10bf3da0c7
xsaven <gms.sw.xsaven@gmail.com>
2021-05-23 17:20:54 +0300

    README.md, config/scaffold.php, src/FileStorage.php, src/LevyModel/LevyModel.php, src/Listeners/MakeRequestListen.php

--- src/Listeners/MakeRequestListen.php
+++ src/Listeners/MakeRequestListen.php
@@ -2,7 +2,11 @@
 
 namespace Bfg\Scaffold\Listeners;
 
+use Bfg\Entity\Core\Entities\DocumentorEntity;
 use Bfg\Scaffold\LevyModel\LevyModel;
+use Bfg\Scaffold\LevyModel\LevyRuleModel;
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
 
 /**
  * Class MakeRequestListen
@@ -18,6 +22,77 @@ class MakeRequestListen extends ListenerControl
      */
     public function handle(LevyModel $model)
     {
-        
+        if ($model->rules && $model->rules->count()) {
+
+            $class = class_entity($model->rules->class_name)
+                ->namespace($model->rules->namespace)
+                ->extend(FormRequest::class);
+
+            $method = $class->method('authorize');
+            $method->line("return true;");
+            $method->doc(function (DocumentorEntity $entity) {
+                $entity->description('Determine if the user is authorized to make this request.');
+                $entity->tagReturn('bool');
+            });
+
+            $rules = [];
+
+            foreach ($model->rules as $rule) {
+                if ($rule->default) {
+                    $rules[$rule->field][] = $rule->rule;
+                } else {
+                    $this->makeRule($rule);
+                    $rules[$rule->field][] = entity("new {$rule->class}");
+                }
+            }
+
+            $method = $class->method('rules');
+            $method->line("return ".array_entity($rules).";");
+            $method->doc(function (DocumentorEntity $entity) {
+                $entity->description('Transform and get a request validated result');
+                $entity->tagReturn('array');
+            });
+
+            $this->storage()->store(
+                app_path("Http/Requests/{$model->rules->class_name}.php"),
+                $class->wrap('php')->render()
+            );
+        }
+
+        $this->storage()->save();
+    }
+
+    /**
+     * @param  LevyRuleModel  $model
+     */
+    protected function makeRule(LevyRuleModel $model)
+    {
+        $class = class_entity($model->class_name)
+            ->implement(Rule::class)
+            ->namespace($model->namespace);
+
+        $method = $class->method('passes');
+        $method->doc(function (DocumentorEntity $entity) {
+            $entity->description('Determine if the validation rule passes.');
+            $entity->tagParam('string', 'attribute');
+            $entity->tagParam('mixed', 'value');
+            $entity->tagReturn('bool');
+        });
+        $method->param('attribute')
+            ->param('value');
+        $method->line('return (string)$value === $value;');
+
+        $method = $class->method('message');
+        $method->doc(function (DocumentorEntity $entity) {
+            $entity->description('Get the validation error message.');
+            $entity->tagReturn('string');
+        });
+        $method->line("return 'The validation error message.';");
+
+        $this->storage()->store(
+            app_path("Rules/{$model->class_name}.php"),
+            $class->wrap('php')->render(),
+            false
+        );
     }
 }
